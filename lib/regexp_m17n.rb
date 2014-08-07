fc29a79002ecc0ff55c145f5031a8e2f3875302f
Nelvin Driz <nelvindriz@gmail.com>
2014-08-07 10:31:05 -0700

    UTF-8 regular expressions fail when working with non UTF-8 strings

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,15 @@
+class String
+  alias_method :original_encode, :encode
+
+  def encode(encoding, *options)
+    original_encode(encoding, *options)
+  rescue Encoding::ConverterNotFoundError
+    force_encoding(encoding)
+  end
+end
+
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    str.encode(Encoding::UTF_8) =~ /^.+$/
   end
 end
